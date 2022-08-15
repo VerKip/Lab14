69249a0b61732288230b1634a0d477f2ff9b8c20
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-15 03:04:39 +0300

    Add project files.

---
+++ Lab14/Animal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    abstract class Bars
+    {
+        public abstract string Name { get; set; }
+
+        public Bars (string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    class Cat : Bars
+    {
+        private string say;
+        private string name;
+        public override string Name 
+        { 
+            get => name; 
+            set => name = value; 
+        }
+
+        public Cat(string name, string say)
+            :base(name)
+        {
+            this.say = say;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+        }
+    }
+
+    class Dog : Bars
+    {
+        private string say;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, string say)
+            : base(name)
+        {
+            this.say = say; 
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+        }
+    }
+}
---
+++ Lab14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat("Кошка", "Мяу");
+            cat1.ShowInfo();
+
+            Dog dog1 = new Dog("Собака", "Гав");
+            dog1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
